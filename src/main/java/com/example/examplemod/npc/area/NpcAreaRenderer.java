17a8b8eb05cabb3d7d2671b4c218d30da14f1803
Erik Sik <grettireirikur@gmail.com>
2023-03-16 23:49:20 +0100

    aslökdjf

--- src/main/java/com/example/examplemod/npc/area/NpcAreaRenderer.java
+++ src/main/java/com/example/examplemod/npc/area/NpcAreaRenderer.java
@@ -2,7 +2,9 @@ package com.example.examplemod.npc.area;
 
 import org.joml.Matrix3f;
 import org.joml.Matrix4f;
+import org.joml.Vector3f;
 
+import com.example.examplemod.setup.Registration;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
@@ -14,6 +16,7 @@ import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
@@ -51,6 +54,8 @@ public class NpcAreaRenderer {
         BlockPos corner2 = area.getCorner2();
         int color = area.getColor();
         if(corner1 == null || corner2 == null) return;
+        Player player = Minecraft.getInstance().player;
+        if(player == null || !player.isHolding(Registration.AREA_DESIGNATOR.get())) return;
 
         Vec3 camPos = camera.getPosition();
 
@@ -63,20 +68,14 @@ public class NpcAreaRenderer {
         int maxZ = Math.max(corner1.getZ(), corner2.getZ());
 
         AABB aabb = new AABB(minX, minY, minZ, maxX, maxY, maxZ);
+        aabb = aabb.inflate(0.1D);
 
         //if(!frustum.isVisible(aabb)) return;
         poseStack.pushPose();
         poseStack.translate(-camPos.x, -camPos.y, -camPos.z);
         VertexConsumer buffer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer(RenderType.lines());
-        //Tesselator tesselator = Tesselator.getInstance();
-        //BufferBuilder buffer = tesselator.getBuilder();
-        RenderSystem.depthMask(true);
         RenderSystem.setShader(GameRenderer::getRendertypeLinesShader);
         RenderSystem.lineWidth(5.0F);
-        //buffer.vertex(0.5D, 15.5D, 0.5D).color(0.9F, 0.9F, 0.0F, 0.2F).endVertex();
-        //buffer.vertex(15.5D, 15.5D, 0.5D).color(0.9F, 0.9F, 0.0F, 0.2F).endVertex();
-        //buffer.vertex(15.5D, 15.5D, 15.5D).color(0.9F, 0.9F, 0.0F, 0.2F).endVertex();
-        //buffer.vertex(0.5D, 15.5D, 15.5D).color(0.9F, 0.9F, 0.0F, 0.2F).endVertex();
         LevelRenderer.renderLineBox(poseStack, buffer, aabb, 1,1,1,1);
 
         renderTopSide(aabb);
@@ -85,18 +84,17 @@ public class NpcAreaRenderer {
         renderBackSide(aabb);
         renderLeftSide(aabb);
         renderRightSide(aabb);
+        
         poseStack.popPose();
     }
 
     public void renderTopSide(AABB aabb) {
         VertexConsumer buffer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer(RenderType.solid());
         RenderSystem.setShader(GameRenderer::getRendertypeSolidShader);
-        RenderSystem.depthMask(true);
         RenderSystem.disableCull();
 
         Matrix4f matrix4f = poseStack.last().pose();
         Matrix3f matrix3f = poseStack.last().normal();
-
         buffer.vertex(matrix4f, (float)aabb.minX, (float)aabb.maxY, (float)aabb.minZ)
                .color(0.5F, 0.5F, 0.5F, 0.5F)
                .uv(0, 0)
@@ -127,12 +125,10 @@ public class NpcAreaRenderer {
     public void renderBottomSide(AABB aabb) {
         VertexConsumer buffer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer(RenderType.solid());
         RenderSystem.setShader(GameRenderer::getRendertypeSolidShader);
-        RenderSystem.depthMask(true);
         RenderSystem.disableCull();
 
         Matrix4f matrix4f = poseStack.last().pose();
         Matrix3f matrix3f = poseStack.last().normal();
-
         buffer.vertex(matrix4f, (float)aabb.minX, (float)aabb.minY, (float)aabb.minZ)
                .color(0.5F, 0.5F, 0.5F, 0.5F)
                .uv(0, 0)
@@ -163,12 +159,10 @@ public class NpcAreaRenderer {
     public void renderLeftSide(AABB aabb) {
         VertexConsumer buffer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer(RenderType.solid());
         RenderSystem.setShader(GameRenderer::getRendertypeSolidShader);
-        RenderSystem.depthMask(true);
         RenderSystem.disableCull();
 
         Matrix4f matrix4f = poseStack.last().pose();
         Matrix3f matrix3f = poseStack.last().normal();
-
         buffer.vertex(matrix4f, (float)aabb.minX, (float)aabb.minY, (float)aabb.minZ)
                .color(0.5F, 0.5F, 0.5F, 0.5F)
                .uv(0, 0)
@@ -199,12 +193,10 @@ public class NpcAreaRenderer {
     public void renderRightSide(AABB aabb) {
         VertexConsumer buffer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer(RenderType.solid());
         RenderSystem.setShader(GameRenderer::getRendertypeSolidShader);
-        RenderSystem.depthMask(true);
         RenderSystem.disableCull();
 
         Matrix4f matrix4f = poseStack.last().pose();
         Matrix3f matrix3f = poseStack.last().normal();
-
         buffer.vertex(matrix4f, (float)aabb.maxX, (float)aabb.minY, (float)aabb.minZ)
                .color(0.5F, 0.5F, 0.5F, 0.5F)
                .uv(0, 0)
@@ -235,12 +227,10 @@ public class NpcAreaRenderer {
     public void renderFrontSide(AABB aabb) {
         VertexConsumer buffer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer(RenderType.solid());
         RenderSystem.setShader(GameRenderer::getRendertypeSolidShader);
-        RenderSystem.depthMask(true);
         RenderSystem.disableCull();
 
         Matrix4f matrix4f = poseStack.last().pose();
         Matrix3f matrix3f = poseStack.last().normal();
-
         buffer.vertex(matrix4f, (float)aabb.minX, (float)aabb.minY, (float)aabb.minZ)
                .color(0.5F, 0.5F, 0.5F, 0.5F)
                .uv(0, 0)
@@ -271,12 +261,10 @@ public class NpcAreaRenderer {
     public void renderBackSide(AABB aabb) {
         VertexConsumer buffer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer(RenderType.solid());
         RenderSystem.setShader(GameRenderer::getRendertypeSolidShader);
-        RenderSystem.depthMask(true);
         RenderSystem.disableCull();
 
         Matrix4f matrix4f = poseStack.last().pose();
         Matrix3f matrix3f = poseStack.last().normal();
-
         buffer.vertex(matrix4f, (float)aabb.minX, (float)aabb.minY, (float)aabb.maxZ)
                .color(0.5F, 0.5F, 0.5F, 0.5F)
                .uv(0, 0)
@@ -303,4 +291,9 @@ public class NpcAreaRenderer {
                .normal(matrix3f, 0, 1, 0)
                .endVertex();
     }
+
+    //public String getSideLookingAt() {
+    //    Vec3 origin = camera.getPosition();
+    //    Vector3f look = camera.getLookVector();
+    //}
 }
